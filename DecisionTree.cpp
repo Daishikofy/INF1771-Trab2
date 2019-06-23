b0e5f5b70740807720c5bc54bdb7d6e2ed3dfa38
Daishikofy <setsuko.daishi@gmail.com>
2019-06-23 20:05:51 -0300

    Entropy + Split + BestParameter implemented and tested

--- DecisionTree.cpp
+++ DecisionTree.cpp
@@ -15,27 +15,164 @@
 #include <vector>
 #include <iostream>
 #include <sstream>
+#include <math.h>
 #include "ReadDataBase.h"
+
+typedef struct instance
+{
+	std::string name;
+	int number;
+}Instance;
+
+typedef std::vector<std::vector<std::vector<std::string>>> Children;
+typedef std::vector<std::vector<std::string>> Group;
+
+float Entropy (Group leaf);
+Children Split (Group node, int parameter);
+void PrintSplit(Children split, int parameter);
+Children SelectBestParameter(Group node, std::vector<int>& parameters);
 
 int main (void)
 {
 	std::string filePath = "car-2.data";
-	std::vector<std::vector<std::string>> dataSet;
+	Group dataSet, auxDebug;
 	CreateDataSet createDataSet;
+	std::vector<int> parameters;
+	for (int i = 0; i < 6; i++)
+		parameters.push_back(i);
 
 	createDataSet.ReadFile (filePath);
 	createDataSet.get(dataSet);
+	int a = 1;
+	for (int i = dataSet.size() - 10; i < dataSet.size() ; i++)
+	{
+		auxDebug.push_back(dataSet[i]);
+		std::cout << "El: " << a << " " << dataSet[i][3] << "\n";
+		a++;
+	}
+
+	Children children = SelectBestParameter(auxDebug, parameters);
+	PrintSplit(children, 6);
 
 	return 0;
 }
 
-int Entropy ()
+float Entropy (Group leaf)
 {
-	int size;
+	int flag;
+	int leafSize = leaf.size();
+	int instanceSize = leaf[0].size();
+	std::vector<Instance> nInstances;
+	Instance instance;
 	
-	//Split the examples by final results
-	//Cacul the probability of each result
-	//Multiply the probability by the log of this probability
-	//Sum everything 
+	instance.name = leaf[0][instanceSize - 1];
+	instance.number = 1;
+	nInstances.push_back(instance);
+
+	for (int i = 1; i < leafSize ; i++)
+	{
+		flag = 0;
+		for (int j = 0; j < nInstances.size(); j++)
+		{
+			if (leaf[i][instanceSize - 1] == nInstances[j].name)
+			{
+				nInstances[j].number += 1;
+				flag = 1;
+			}
+		}
+		if (!flag)
+		{
+			instance.name = leaf[i][instanceSize - 1];
+			instance.number = 1;
+			nInstances.push_back(instance);
+		}
+	}
+
+	float probability, sum = 0;
+	for (int i = 0; i < nInstances.size(); i++)
+	{
+		probability = (float)nInstances[i].number/(float)leafSize;
+		sum += probability*std::log(probability);
+	}
+	
+	return std::abs(sum);
 }
 
+Children Split (Group node, int parameter)
+{
+	int flag;
+	int nodeSize = node.size();
+	Children children;
+
+	for (int i = 0; i < nodeSize ; i++)
+	{
+		flag = 0;
+		for (int j = 0; j < children.size(); j++)
+		{
+			if (node[i][parameter] == children[j][0][parameter])
+			{
+				children[j].push_back(node[i]);
+				flag = 1;
+				break;
+			}
+		}
+		if (!flag)
+		{
+			std::vector<std::vector<std::string>> group;
+			group.push_back(node[i]);
+			children.push_back(group);
+		}
+	}
+
+	return children;
+}
+
+Children SelectBestParameter(Group node, std::vector<int>& parameters)
+{
+	//Para todos os parametros na lista fazer split
+	//Cacular a entropia de todos os filhos
+	//Adicionar a entropia
+	//Comparar com o melhor
+	//Se o atual for melhor do que o melhor
+		//O Children best passa a ser o atual
+		//O indice do melhor parametro passa  ser o atual
+	//Remove o melhor parametro da lista de parametros
+	//retorna o children
+	int bestParamIndex = 0;
+	Children bestChildren = Split(node, parameters[bestParamIndex]);
+
+	float bestEntropy = 0;
+	for (int i = 0 ; i < bestChildren.size(); i++)
+		bestEntropy += Entropy(bestChildren[i]);
+
+	for (int i = 1 ; i < parameters.size(); i++)
+	{
+		Children children = Split (node, parameters[i]);
+
+		float entropy = 0;
+		for (int j = 0 ; j < children.size(); j++)
+			entropy += Entropy(children[j]);
+
+		if (entropy < bestEntropy)
+		{
+			bestParamIndex = i;
+			bestChildren = children;
+			bestEntropy = entropy;
+		}
+	}
+	std::cout << "P: " << bestParamIndex << "\n";
+	parameters.erase(parameters.begin() + bestParamIndex);
+	return bestChildren;
+}
+
+void PrintSplit(Children split, int parameter)
+{
+	for (int i = 0; i < split.size(); i++)
+	{
+		std::cout << "\nGROUP " << i << "\n";
+		for (int j = 0; j < split[i].size(); j++)
+		{
+			std::cout << j << " - " << split[i][j][parameter] << "\n";
+		}
+	}
+}
