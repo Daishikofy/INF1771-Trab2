670b56588451dbe498dd7f44de0a811160d6f54b
Daishikofy <setsuko.daishi@gmail.com>
2019-06-23 00:21:26 -0300

    Read the file and save it's information in a structure

---
+++ ReadDataBase.cpp
@@ -0,0 +1,64 @@
+/*****
+
+Creator: Lola Magnard
+Description of the following code : 
+
+	The purpose of this code is reading the data from the UCI repository : Car evaluation
+	- An exemple is stored in a vector of string 
+	- Each vector contains 6 spaces corresponding to the 6 parameters
+	- The whole database is stored in a vector<vector<std::string>>
+
+Class values:
+	unacc, acc, good, vgood
+
+Attributes
+	buying:   vhigh, high, med, low.
+	maint:    vhigh, high, med, low.
+	doors:    2, 3, 4, 5more.
+	persons:  2, 4, more.
+	lug_boot: small, med, big.
+	safety:   low, med, high.
+
+*****/
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "ReadDataBase.h"
+
+void CreateDataSet::ReadFile (std::string filePath)
+{
+	std::string line;
+	std::ifstream file (filePath);
+	int debug = 0;
+
+	if (! file.is_open ())
+	{
+		std::cout << "Couldn't open de file\n";
+		return;
+	}
+
+	while ( getline (file, line))
+	{
+		std::vector<std::string> example;
+		std::istringstream lineStream(line);
+		std::string token;
+
+		//Fill the exemple with all its attributes
+		while(std::getline(lineStream, token, ',')) 
+		{
+			example.push_back(token);
+		}
+
+		//Add the exemple to the current dataSet
+		_dataSet.push_back(example);
+		debug ++;
+	}
+
+	std::cout << _dataSet[3][5] << "\n";
+}
+
+void CreateDataSet::get(std::vector<std::vector<std::string>>& dataSet)
+{
+	dataSet = _dataSet;
+}
---
+++ ReadDataBase.h
@@ -0,0 +1,34 @@
+/*****
+
+Creator: Lola Magnard
+Description of the following code : 
+
+	The purpose of this code is reading the data from the UCI repository : Car evaluation
+	- An exemple is stored in a vector of string 
+	- Each vector contains 6 spaces corresponding to the 6 parameters
+	- The whole database is stored in a vector<vector<std::string>>
+
+Class values:
+	unacc, acc, good, vgood
+
+Attributes
+	buying:   vhigh, high, med, low.
+	maint:    vhigh, high, med, low.
+	doors:    2, 3, 4, 5more.
+	persons:  2, 4, more.
+	lug_boot: small, med, big.
+	safety:   low, med, high.
+
+*****/
+#pragma once
+#include <vector>
+
+class CreateDataSet
+{
+public:
+	void ReadFile (std::string filePath);
+	void get(std::vector<std::vector<std::string>>& dataSet);
+
+private:
+	std::vector<std::vector<std::string>> _dataSet;
+};
